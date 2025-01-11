c53b640b753584c3227148d24e1aa33db681696a
Dan Buxton <dan.buxton99@gmail.com>
2025-01-11 19:59:03 +0000

    Added more tests

--- JokeAPIWrapper.Tests/ApiClientTests.cs
+++ JokeAPIWrapper.Tests/ApiClientTests.cs
@@ -4,23 +4,54 @@ namespace JokeAPIWrapper.Tests;
 
 public class ApiClientTests
 {
-    private readonly ApiClientV2 _client;
+    internal readonly ApiClientV2 _client;
     public ApiClientTests()
     {
         _client = new ApiClientV2(new HttpClient());
     }
 
+    [Fact]
+    public async Task GetJokeAnyCategoryAsyncTest()
+    {
+        var builder = new RequestBuilder();
+
+        var joke = await _client.GetJokeAsync(builder);
+
+        Assert.NotNull(joke);
+        Assert.False(joke.Error);
+    }
     [Theory]
     [InlineData(JokeCategory.Spooky)]
     [InlineData(JokeCategory.Misc)]
     [InlineData(JokeCategory.Programming)]
     [InlineData(JokeCategory.Pun)]
     [InlineData(JokeCategory.Dark)]
-    public async Task GetJokeCategoryTest(JokeCategory targetCategory)
+    public async Task GetJokeCategoryAsyncTest(JokeCategory targetCategory)
     {
-        var joke = await _client.GetJokeAsync(targetCategory);
+        IRequestBuilder builder = new RequestBuilder();
+        builder.WithCategory(targetCategory);
+
+        var joke = await _client.GetJokeAsync(builder);
 
         Assert.Equal(joke!.GetCategory(), targetCategory);
+        Assert.False(joke.Error);
+    }
+
+    [Theory]
+    [InlineData(JokeLanguage.EN)]
+    [InlineData(JokeLanguage.PT)]
+    [InlineData(JokeLanguage.FR)]
+    [InlineData(JokeLanguage.DE)]
+    [InlineData(JokeLanguage.ES)]
+    [InlineData(JokeLanguage.CS)]
+    public async Task GetJokeAltLanguageAsyncTest(JokeLanguage targetLanguage)
+    {
+        IRequestBuilder builder = new RequestBuilder();
+        builder.WithLanguage(targetLanguage);
+
+        var joke = await _client.GetJokeAsync(builder);
+
+        Assert.Equal(joke!.Lang, targetLanguage.ToString().ToLower());
         Assert.False(joke.Error);
     }
 
@@ -29,28 +60,13 @@ public class ApiClientTests
     [InlineData(JokeCategory.Programming, JokeCategory.Pun, JokeCategory.Dark)]
     public async Task GetJokeMultiCategoryTest(params JokeCategory[] targetCategories)
     {
-        var joke = await _client.GetJokeAsync(targetCategories);
+        IRequestBuilder builder = new RequestBuilder();
+        builder.WithCategories(targetCategories);
+
+        var joke = await _client.GetJokeAsync(builder);
         var cat = joke!.GetCategory();
 
         Assert.Contains(cat, targetCategories);
-        Assert.False(joke.Error);
-    }
-
-    [Fact]
-    public async Task GetJokeAnyCategoryAsyncTest()
-    {
-        var joke = await _client.GetJokeAsync();
-
-        Assert.NotNull(joke);
-        Assert.False(joke.Error);
-    }
-
-    [Fact]
-    public void GetJokeAnyCategoryTest()
-    {
-        var joke = _client.GetJoke();
-
-        Assert.NotNull(joke);
         Assert.False(joke.Error);
     }
 }
